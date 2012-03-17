117d607500ef6c8769d957f83cedb0f0fa9e0ff3
Bo Zhang <object.new@gmail.com>
2012-03-17 15:23:44 +0800

    init rails env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProtoMonitor::Application.config.secret_token = '7479c6ffd63e75ff1bc446fa53dfbc3fb632b074efcca584f33567370b4957b036bd0d1126196be639f8e387f4328ca3675d59ea433d3bbcdec3e81d0b1093f8'
